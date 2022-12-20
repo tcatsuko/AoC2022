74ec035dfb3ca8b55ce2946f370ea632bd21d1fb
tcatsuko <58714618+tcatsuko@users.noreply.github.com>
2022-12-19 20:14:42 -0600

    Day 17: Pyroclastic Flow

--- AoC2022/aoc17.py
+++ AoC2022/aoc17.py
@@ -4,6 +4,7 @@ instructions = ''
 for line in f:
     instructions = line.strip()
 f.close()
+inst_length = len(instructions)
 shapes = []
 heights = []
 heights += [0]
@@ -16,16 +17,21 @@ wind_offset = 0
 wind_repeated = False
 height = 0
 chamber_max_height = 0
-pieces = 1000000000000
+found_one_cycle = False
+pieces =100000
+found_cycle_counter = 0
+#pieces = 
 elapsed_pieces = 0
 found_cycle = False
 inst_length = len(instructions)
+cycle_height_list = []
+cycle_start_list = []
 for piece_counter in range(pieces):
-
+    cycle_key = (shape_offset, wind_offset)
     # Housekeeping
-    if len(chamber) > 100:
-        chamber_offset += len(chamber) - 100
-        for x in range(len(chamber)-100):
+    if len(chamber) > 2000:
+        chamber_offset += len(chamber) - 2000
+        for x in range(len(chamber)-2000):
             chamber.pop(0)
     
     
@@ -131,7 +137,7 @@ for piece_counter in range(pieces):
             shape_offset += 1
             if shape_offset == len(shapes):
                 shape_offset = 0
-            cycle_key = (shape_offset, wind_offset)
+            
             heights += [chamber_max_height + chamber_offset]
             
             if wind_repeated == True:
@@ -142,39 +148,74 @@ for piece_counter in range(pieces):
                     cycle_dictionary[cycle_key]['height'] = chamber_max_height + chamber_offset
                     cycle_dictionary[cycle_key]['prev_height'] = prev_height
                     cycle_dictionary[cycle_key]['prev_chamber'] = prev_chamber
+                    
                 else:
                     if cycle_dictionary[cycle_key]['chamber'] == chamber:
-                        # we have a cycle
+
+
+                        cycle_history = cycle_dictionary[cycle_key]
+                        cycle_history_pre_height = heights[cycle_history['pieces'] - 1]
+                        cycle_start_height = heights[cycle_history['pieces']]
+                        cycle_end_height = heights[piece_counter - 1]
+
                         cycle_start_height = cycle_dictionary[cycle_key]['height']
-                        cycle_height = prev_height - cycle_start_height 
+                        cycle_height = cycle_end_height - cycle_start_height
                         cycle_start_pieces = cycle_dictionary[cycle_key]['pieces']
                         cycle_length = piece_counter - cycle_start_pieces - 1
                         cycle_prev_chamber = cycle_dictionary[cycle_key]['prev_chamber']
+                        alt_cycle_height = heights[piece_counter - 1] - heights[cycle_start_pieces] 
+                        cycle_end = piece_counter - 1
                         found_cycle = True
+                        
+                        if cycle_key == (1,10086): # In my input this is the first detected cycle
+                            #cycle_start_list += [cycle_history['pieces']  + len(cycle_height_list) * cycle_length]
+                            if len(cycle_height_list) < 1:
+                                cycle_height_list += [cycle_height]
+                                cycle_start_list += [cycle_history['pieces']]
+                            else:
+                                cycle_height_list += [cycle_height - sum(cycle_height_list)]
+                                
+                            cycle_start_list += [piece_counter]
+
                         
         else:
             shape_start -= 1
-    #if found_cycle == True:
-        #break
-# Determine full cycles + whatever else is needed to get to 
-total_cycles_needed = 1000000000000
-pre_cycle = cycle_start_pieces - 1
-remaining_cycles = total_cycles_needed - pre_cycle
+    # if found_cycle == True:
+    #     break
 
-#remaining_cycles = total_cycles_needed - cycle_start_pieces - 1
+total_cycles_needed = 2022
+
+pre_cycle = cycle_start_list[1] - 1
+remaining_cycles = total_cycles_needed - pre_cycle
+cycle_length = cycle_start_list[-1]- cycle_start_list[-2]
+
 full_cycles = remaining_cycles // cycle_length
 extra_iterations = remaining_cycles % cycle_length 
 
 pieces = extra_iterations
 cycle_detect_current_height = chamber_max_height + chamber_offset
 
-initial_height = heights[cycle_start_pieces - 1]
-added_cycle_height = full_cycles * cycle_height
-post_cycle_height = heights[cycle_start_pieces - 1 + extra_iterations] - heights[cycle_start_pieces - 1] 
-
-
+initial_height = heights[pre_cycle]
+added_cycle_height = full_cycles * cycle_height_list[-1]
+post_cycle_height = heights[pre_cycle + extra_iterations] - heights[pre_cycle] 
 
 total_height = initial_height + added_cycle_height + post_cycle_height
 print('Part 1: chamber max height after 2022 pieces is ' + str(total_height))
-# 1596903669743 too high
-# 1597477064237 too high
+total_cycles_needed = 1000000000000
+
+pre_cycle = cycle_start_list[1] - 1
+remaining_cycles = total_cycles_needed - pre_cycle
+cycle_length = cycle_start_list[-1]- cycle_start_list[-2]
+
+full_cycles = remaining_cycles // cycle_length
+extra_iterations = remaining_cycles % cycle_length 
+
+pieces = extra_iterations
+cycle_detect_current_height = chamber_max_height + chamber_offset
+
+initial_height = heights[pre_cycle]
+added_cycle_height = full_cycles * cycle_height_list[-1]
+post_cycle_height = heights[pre_cycle + extra_iterations] - heights[pre_cycle] 
+
+total_height = initial_height + added_cycle_height + post_cycle_height
+print('Part 2: chamber max height after 1000000000000 pieces is ' + str(total_height))
